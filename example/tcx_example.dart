95307b025a03bb004bae414af8391f019667c2e3
Rodrigo Bernardo Pimentel <rbp@isnomore.net>
2021-06-06 13:47:24 +0200

    Re-initing, with changed repo name

---
+++ example/tcx_example.dart
@@ -0,0 +1,6 @@
+import 'package:tcx/tcx.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ lib/tcx.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library tcx;
+
+export 'src/tcx_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
